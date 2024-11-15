2c0b43e18ed97907f6dfdc7c79c9430c211c436c
Louieeeeeee911 <lmacabuhay2018@gmail.com>
2024-11-15 09:27:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import oop as ab
+
+leonardo = ab.Leonardo("Leonardo", "Leo")
+michaelangelo = ab.Michaelangelo("Michaelangelo", "Gelo")
+donatello = ab.Donatello("Donatello", "Dona")
+raphael = ab.Raphael("Raphael", "Paeng")
+
+print(leonardo.alias)
+print(michaelangelo.alias)
+print(donatello.alias)
+print(raphael.alias)
