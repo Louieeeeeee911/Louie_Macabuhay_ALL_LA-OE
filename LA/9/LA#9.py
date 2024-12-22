f06ff1a589b0f99412a7cd331b137a325e292344
Louieeeeeee911 <lmacabuhay2018@gmail.com>
2024-12-22 15:46:17 +0800

    Create LA#9.py

---
+++ LA/9/LA#9.py
@@ -0,0 +1,12 @@
+class car:
+    def __init__(self, brand):
+        self.brand = brand
+    
+    def __str__(self):
+        return f"your car brand is {car1.brand}"
+
+car1 = car("toyota")
+del car1
+print(car1)
+    
+    
